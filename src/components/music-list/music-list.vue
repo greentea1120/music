d634e0d50d76eab9e42af888e8e781af833d8f96
Greentea1120 <741024920@qq.com>
2017-11-29 01:25:49 +0800

    6-10~6-14

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
-      <div class="filter"></div>
+      <div class="play-wrapper">
+        <div class="play" ref="playBtn">
+          <i class="icon-play" v-show="songs.length>0" ></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
+      <div class="filter" ref="filter"></div>
     </div>
-    <scroll :data="songs" class="list" ref="list">
+    <div class="bg-layer" ref="layer"></div>
+    <scroll @scroll="scroll" :probe-type="probeType" :listen-scroll="listenScroll" :data="songs" class="list" ref="list">
       <div class="song-list-wrapper">
         <song-list :songs="songs"></song-list>
+      </div>
+      <div class="loading-container" v-show="!songs.length">
+        <loading></loading>
       </div>
     </scroll>
   </div>
@@ -18,9 +28,18 @@
 <script type="text/ecmascript-6">
   import Scroll from 'base/scroll/scroll'
   import SongList from 'base/song-list/song-list'
+  import {prefixStyle} from 'common/js/dom'
+  import Loading from 'base/loading/loading'
+
+  const RESERVED_HEIGHT = 40
+  const transform = prefixStyle('transform')
+  const backdrop = prefixStyle('backdrop-filter')
+
   export default {
     created() {
       console.log(this.bgImage)
+      this.probeType = 3
+      this.listenScroll = true
     },
     props: {
       bgImage: {
@@ -36,14 +55,15 @@
         default: ''
       }
     },
-    data () {
+    data() {
       return {
-
+        scrollY: 0
       }
     },
     components: {
       Scroll,
-      SongList
+      SongList,
+      Loading
     },
     computed: {
       bgStyle() {
@@ -51,7 +71,49 @@
       }
     },
     mounted() {
+      this.imageHeight = this.$refs.bgImage.clientHeight
+      this.minTranslateY = -this.imageHeight + RESERVED_HEIGHT
       this.$refs.list.$el.style.top = `${this.$refs.bgImage.clientHeight}px`
+    },
+    methods: {
+      scroll(pos) {
+        this.scrollY = pos.y
+      },
+      back() {
+        this.$router.back()
+      }
+    },
+    watch: {
+      scrollY(newY) {
+        let translateY = Math.max(this.minTranslateY, newY)
+        let zIndex = 0
+        let scale = 1
+        let blur = 0
+        this.$refs.layer.style[transform] = `translate3d(0,${translateY}px,0)`
+        // this.$refs.layer.style['webkitTransform'] = `translate3d(0,${newY}px,0)`
+        const percent = Math.abs(newY / this.imageHeight)
+        if (newY > 0) {
+          scale = 1 + percent
+          zIndex = 10
+        } else {
+          blur = Math.min(20 * percent, 20)
+        }
+        this.$refs.filter.style[backdrop] = `blur(${blur}px)`
+        // this.$refs.filter.style['webkitBackdrop-filter'] = `blur(${blur}px)`
+        if (newY < this.minTranslateY) {
+          zIndex = 10
+          this.$refs.bgImage.style.paddingTop = 0
+          this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+          this.$refs.playBtn.style.display = 'none'
+        } else {
+          this.$refs.bgImage.style.paddingTop = '70%'
+          this.$refs.bgImage.style.height = 0
+          this.$refs.playBtn.style.display = ''
+        }
+        this.$refs.bgImage.style.zIndex = zIndex
+        this.$refs.bgImage.style[transform] = `scale(${scale})`
+        // this.$refs.bgImage.style['webkitTransform'] = `scale(${scale})`
+      }
     }
   }
 </script>
@@ -133,6 +195,7 @@
       background: $color-background
     .list
       position: absolute
+      // overflow: hidden
       top: 0
       bottom: 0
       width: 100%
